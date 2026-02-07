170541c99d7c22e94e14f5898dcc4963d15ae6a8
jean-charles.verdier <jean-charles.verdier@flare.io>
2026-02-07 11:15:39 -0500

    removed linux/nvim

--- linux/nvim/lua/config/plugins.lua
+++
@@ -1,5 +0,0 @@
-m-- use fzf in telescope
-require("telescope").load_extension("fzf")
-
--- displays which-key gui
-vim.api.nvim_set_keymap("n", "<leader>?", require("which-key").show({ global = false }))
--- linux/nvim/lua/plugins/harpoon.lua
+++
@@ -1,6 +0,0 @@
--- Quick navigation between frequently used files
-return {
-	"ThePrimeagen/harpoon",
-	branch = "harpoon2",
-	dependencies = { "nvim-lua/plenary.nvim" },
-}
